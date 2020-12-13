45649aa76e09b18fc0b242de0b5d54683a020016
chachr09 <71359981+chachr09@users.noreply.github.com>
2020-12-12 21:39:42 -0800

    updated the day and time in JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+function dateTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = currentDateTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDateTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[currentDateTime.getDay()];
+
+  let dateElement = document.querySelector(`#currentDateTime`);
+  dateElement.innerHTML = dateTime(response.data.dt * 1000);
+  return `${day} ${hours}:${minutes}`;
+}
+
 function formatDate() {
   let days = [
     `Sunday`,
@@ -34,11 +60,11 @@ function formatDate() {
   let currentYear = currentTime.getFullYear();
 
   let h3 = document.querySelector(`h3`);
-  h3.innerHTML = `${currentHours}:${currentMinutes}`;
+  h3.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
   let h4 = document.querySelector(`h4`);
-  h4.innerHTML = `${currentDay}. ${currentMonth}. ${currentDate}, ${currentYear}`;
+  h4.innerHTML = `${currentMonth}. ${currentDate}, ${currentYear}`;
 
-  let formattedDate = `${currentHours}:${currentMinutes} ${currentDay}. ${currentMonth} ${currentDate}, ${currentYear} `;
+  let formattedDate = `${currentDay} ${currentHours}:${currentMinutes}  ${currentMonth}. ${currentDate}, ${currentYear} `;
   console.log(formattedDate);
 
   return formattedDate;
